d1e0cfd0cf6534c026e7ad7e7c25b26271323466
Jason Seminara <jseminara@gmail.com>
2016-10-09 21:53:51 -0400

    converted code to OOP class-based

--- randomizer.js
+++ randomizer.js
@@ -1,55 +1,59 @@
-function studentRandomizer(f = 0) {
-  let failures = f;
-  /* Self-contained closure that keeps track of how many times an instructor was chosen */
-  function instructorChoice(leadCount, maxCount) {
-    const leads = new Array(leadCount).fill(maxCount);
+class BadSquadConfig extends Error {
+  constructor() {
+    super('Could Not find a suitable squad layout');
+  }
+}
 
-    const use = (pos) => {
-      const r = (leads[pos] > 0) ? leads[pos] -= 1 : false;
-      return r;
-    };
-
-    const getNext = () => {
-      let pos;
-      // gen a random number
-      while (leads.some(x => x > 0)) {
-        pos = Math.floor(Math.random() * leadCount);
-        // console.log(pos, this.leads);
-        if (leads[pos]) break;
-      }
-      return pos;
-    };
-
-    const getValidToken = (student, n = 0) => {
-      const id = getNext();
-      // console.log('getValidToken', student, n, id)
-      if (n > 100) {
-        failures += 1;
-        return 9;
-      }
-
-      if (!student.includes(id)) {
-        use(id);
-        return id;
-      }
-
-      return getValidToken(student, n + 1);
-    };
-
-    /* Once a number is chosen, we're free to use it */
-    return { getNext, getValidToken };
+class InstructorChoice {
+  constructor(leadCount, maxCount) {
+    this.leadCount = leadCount;
+    this.leads = new Array(leadCount).fill(maxCount);
   }
 
-  function randomizeSquads(students, leaderCount, rotations) {
-    let r = rotations;
-    let studentRotations = new Array(students).fill(9).map(() => []);
-    const MAX_SQUAD_SIZE = Math.floor(students / leaderCount);
+  use(pos) {
+    const r = (this.leads[pos] > 0) ? this.leads[pos] -= 1 : false;
+    return r;
+  }
 
-    while (r > 0) {
-      r -= 1;
-      const leaderTokenCounter = instructorChoice(leaderCount, MAX_SQUAD_SIZE);
+  getNext() {
+    let pos;
+    // gen a random number
+    while (this.leads.some(x => x > 0)) {
+      pos = Math.floor(Math.random() * this.leadCount);
+      // console.log(pos, this.leads);
+      if (this.leads[pos]) break;
+    }
+    return pos;
+  }
 
-      studentRotations = studentRotations.map((student) => {
+  getValidToken(student, n = 0) {
+    const id = this.getNext();
+    // console.log('getValidToken', student, n, id)
+    if (n > 100) throw new BadSquadConfig();
+
+    if (!student.includes(id)) {
+      this.use(id);
+      return id;
+    }
+
+    return this.getValidToken(student, n + 1);
+  }
+}
+
+class Squads {
+  constructor(students, leaderCount, r) {
+    this.leaderCount = leaderCount;
+    this.rotations = r;
+    this.studentRotations = new Array(students).fill(9).map(() => []);
+    this.MAX_SQUAD_SIZE = Math.ceil(students / leaderCount);
+  }
+
+  randomize() {
+    while (this.rotations > 0) {
+      this.rotations -= 1;
+      const leaderTokenCounter = new InstructorChoice(this.leaderCount, this.MAX_SQUAD_SIZE);
+
+      this.studentRotations = this.studentRotations.map((student) => {
         const id = leaderTokenCounter.getValidToken(student);
         student.push(id);
         // console.log(i,student)
@@ -57,21 +61,41 @@ function studentRandomizer(f = 0) {
       });
     }
 
-    return studentRotations;
+    return this.studentRotations;
   }
-
-  function execute(s, l, r) {
-    let results;
-    do {
-      failures = 0;
-      results = randomizeSquads(s, l, r);
-    } while (failures);
-
-    return results.sort((a, b) => (+a.join('') - +b.join('')));
-  }
-
-  return { execute };
 }
 
 
-console.log(studentRandomizer().execute(55, 5, 4));
+class StudentRandomizer {
+  constructor(f = 0) {
+    this.failures = f;
+  }
+  /* Self-contained closure that keeps track of how many times an instructor was chosen */
+
+  execute(s, l, r) {
+    const sort2DArray = (a, b) => +a.join('') - +b.join('');
+
+    let results;
+    let attempts = 0;
+
+    do {
+      this.failures = 0;
+      attempts += 1;
+      try {
+        results = new Squads(s, l, r).randomize();
+      } catch (e) {
+        if (!(e instanceof BadSquadConfig)) {
+          throw e;
+        }
+        this.failures += 1;
+      }
+    } while (this.failures);
+
+    console.log('attempts', attempts);
+    return results.sort(sort2DArray);
+  }
+
+}
+
+
+console.log(new StudentRandomizer().execute(56, 5, 4));
